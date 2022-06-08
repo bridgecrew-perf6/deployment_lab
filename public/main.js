15d2441c2df72f6247f46b053b1fa79b248e19cd
satch-mo <satchel0293@gmail.com>
2022-06-08 15:32:55 -0500

    character function

--- public/main.js
+++ public/main.js
@@ -1,10 +1,13 @@
-writeRandomQuote = function () {
-  var quotes = new Array();
-  quotes[0] = "Prabaker";
-  quotes[1] = "Abdel Khader Khan";
-  quotes[2] = "Linbaba";
-  quotes[3] = "Karla";
-  var rand = Math.floor(Math.random() * quotes.length);
-  document.write(quotes[rand]);
+writeRandomCharacter = function () {
+  let characters = new Array();
+  characters[0] = "Prabaker";
+  characters[1] = "Abdel Khader Khan";
+  characters[2] = "Linbaba";
+  characters[3] = "Karla";
+  characters[4] = "Raheem";
+  characters[5] = "Hassan Obikwa";
+  characters[6] = "Habib Adbur Rahman";
+  let rand = Math.floor(Math.random() * characters.length);
+  document.write(characters[rand]);
 };
-writeRandomQuote();
+writeRandomCharacter();
