1ee713e7cb39fd5138e7d7dd9e8ff97675ec593a
satch-mo <satchel0293@gmail.com>
2022-06-08 15:28:46 -0500

    character function

--- public/main.js
+++ public/main.js
@@ -1,12 +1,9 @@
 writeRandomQuote = function () {
   var quotes = new Array();
-  quotes[0] = "Action is the real measure of intelligence.";
-  quotes[1] =
-    "Baseball has the great advantage over cricket of being sooner ended.";
-  quotes[2] =
-    "Every goal, every action, every thought, every feeling one experiences, whether it be consciously or unconsciously known, is an attempt to increase one's level of peace of mind.";
-  quotes[3] =
-    "A good head and a good heart are always a formidable combination.";
+  quotes[0] = "Prabaker";
+  quotes[1] = "Abdel Khader Khan";
+  quotes[2] = "Linbaba";
+  quotes[3] = "Karla";
   var rand = Math.floor(Math.random() * quotes.length);
   document.write(quotes[rand]);
 };
