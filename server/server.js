e91720e0bb0367ad85cbbf2ad717492c85ea6a6b
satch-mo <satchel0293@gmail.com>
2022-06-08 15:25:19 -0500

    added js

--- server/server.js
+++ server/server.js
@@ -4,10 +4,15 @@ const path = require("path");
 const app = express();
 
 app.use(express.static("public"));
+// app.use("/js", express.static(path.join(__dirname, "public/main.js")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 }); // '/' general file path
+
+// app.get("/js", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./public/main.js"));
+// });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on ${port}`));
